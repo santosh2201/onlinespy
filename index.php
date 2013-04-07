c3f5f3972da3a38e620530ef6cb17671c7021395
santosh2201 <santosh.cool.reddy@gmail.com>
2013-04-07 14:10:37 +0530

    hgkgnknr

--- index.php
+++ index.php
@@ -44,7 +44,7 @@
   echo '<pre>';
  // print_r("multi query results:");
 print_r("I have given a limit value of 10 so only recent 10 news feeds will be shown, we can increase this value");
-  print_r($fql_multiquery_obj);
+  print_r($fql_multiquery_result);
   echo '</pre>';  
 
 
