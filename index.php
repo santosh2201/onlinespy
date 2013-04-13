bd63e0d462f9aca240324fd3ed8e9704a0dddfc6
santosh.cool.reddy@gmail.com <>
2013-04-13 14:00:55 +0000

    efwefe

--- index.php
+++ index.php
@@ -1,24 +1,3 @@
-<?php
-
-  require_once('php-sdk/src/facebook.php');
-
-  $config = array(
-    'appId' => '478174598904856',
-    'secret' => 'e7a9947ac59f9c5a264cd83f68689d80',
-  'sharedSession' => true,
-  'trustForwarded' => true,
-  );
-
-  $facebook = new Facebook($config);
-  $user_id = $facebook->getUser();
-
-print_r($user_id);
- 
-$access_token = $facebook->getAccessToken();
-  print_r($access_token);
-
-?>
-
 
 <?php
   $app_id = '478174598904856';
@@ -59,52 +38,10 @@ $access_token = $facebook->getAccessToken();
   // response is of the format "access_token=AAAC..."
   $access_token = substr(file_get_contents($token_url), 13);
 
-
+      $friendsbday = $facebook->api('/'.$user_id.'?fields=friends.fields(birthday)?access_token='.$access_token); 
+       echo $friendsbday;
 
 
 ?>
 
 
-<html>
-  <head></head>
-  <body>
-
-  <?php
-    if($user_id) {
-
-$friends = $facebook->api('/'.$user_id.'/friends');
-                $friendsList = array();
-                foreach ($friends as $key=>$value) 
-                {
-                   foreach ($value as $fkey=>$fvalue) {
-
-                       $friendsList[] = $fvalue[id];
-//print_r($friendsList);
-                     //$fname= $facebook->api('/'.$fvalue[id].'?fields=name');  
-                     //print_r($fname);    
-                     //$fpic= $facebook->api('/'.$fvalue[id].'?fields=birthday');  
-                     //print_r($fpic);               
-                     
-                   }
-
-                }  
-      
-      $friendsbday = $facebook->api('/'.$user_id.'?fields=friends.fields(birthday)?access_token='.$access_token); 
-       echo $friendsbday;
-
-      // https://graph.facebook.com/oauth/authorize?type=user_agent&client_id=123456789123456&redirect_uri=http%3A%2F%2Fwww.example.com&scope=read_stream,manage_pages,offline_access,user_photos
-      
-    } else {
-
-      // No user, print a link for the user to login
-      $login_url = $facebook->getLoginUrl(array(
-                'scope'         => 'email,offline_access,publish_stream,user_birthday,user_location,user_work_history,user_about_me,user_hometown'
-            ));
-       echo 'Please <a href="' . $login_url . '">login.</a>';
-
-    }
-
-  ?>
-
-  </body>
-</html>
