29b59a9bcfbf9ee1a5348fcca93dd9c157bb3301
santosh.cool.reddy@gmail.com <>
2013-04-13 11:24:48 +0000

    rsghwrsg

--- index2.php
+++ index2.php
@@ -1,50 +1,24 @@
+
+
+
+
 <?php
-  $app_id = '478174598904856';
-  $app_secret = 'e7a9947ac59f9c5a264cd83f68689d80';
-  $my_url = 'https://rocky-woodland-3057.herokuapp.com/';
+  //Get Facebook SDK Object
+  $config = array(
+    'appId'  => '478174598904856',
+    'secret' => 'e7a9947ac59f9c5a264cd83f68689d80',
+    'cookie' => true,
+  );
 
-  $code = $_REQUEST["code"];
+  $facebook = new Facebook($config);
 
- // auth user
- if(empty($code)) {
-    $dialog_url = 'https://www.facebook.com/dialog/oauth?client_id=' 
-    . $app_id . '&redirect_uri=' . urlencode($my_url) ;
-    echo("<script>top.location.href='" . $dialog_url . "'</script>");
-  }
+  //Create Query
+  $params = array(
+      'method' => 'fql.query',
+      'query' => "SELECT uid, pic, pic_square, name FROM user WHERE uid IN (SELECT uid2 FROM friend WHERE uid1 = me())",
+  );
 
-  // get user access_token
-   $token_url = 'https://graph.facebook.com/oauth/access_token?client_id='
-    . $app_id . '&redirect_uri=' . urlencode($my_url) 
-    . '&client_secret=' . $app_secret 
-    . '&code=' . $code;
-
-  // response is of the format "access_token=AAAC..."
-  $access_token = substr(file_get_contents($token_url), 13);
-
-  // run fql query
-  $fql_query_url = 'https://graph.facebook.com/'
-    . 'fql?q=SELECT+uid2+FROM+friend+WHERE+uid1=me()'
-    . '&access_token=' . $access_token;
-  $fql_query_result = file_get_contents($fql_query_url);
-  $fql_query_obj = json_decode($fql_query_result, true);
-
-  // display results of fql query
-  echo '<pre>';
-  print_r("query results:");
-  print_r($fql_query_obj);
-   echo '</pre>';
-
-  // run fql multiquery
-  $fql_multiquery_url = 'https://graph.facebook.com/'
-    . 'fql?q={"all+friends":"SELECT+uid2+FROM+friend+WHERE+uid1=me()",'
-    . '"my+name":"SELECT+name+FROM+user+WHERE+uid=me()"}'
-    . '&access_token=' . $access_token;
-  $fql_multiquery_result = file_get_contents($fql_multiquery_url);
-  $fql_multiquery_obj = json_decode($fql_multiquery_result, true);
-
-  // display results of fql multiquery
-  echo '<pre>';
-  print_r("multi query results:");
-  print_r($fql_multiquery_obj);
-  echo '</pre>';
+  //Run Query
+  $result = $facebook->api($params);
+  print_r($result);
 ?>
