6dec469b7c1bbdb7e637f277f7402db98cbde0bb
santosh.cool.reddy@gmail.com <>
2013-04-13 13:46:10 +0000

    jhg

--- index.php
+++ index.php
@@ -1,4 +1,10 @@
 <?php
+  $app_id = '478174598904856';
+  $app_secret = 'e7a9947ac59f9c5a264cd83f68689d80';
+  $my_url = 'https://rocky-woodland-3057.herokuapp.com/';
+
+  $code = $_REQUEST["code"];
+
 
   require_once('php-sdk/src/facebook.php');
 
@@ -12,58 +18,36 @@
   $facebook = new Facebook($config);
   $user_id = $facebook->getUser();
 
-print_r($user_id);
- 
-$access_token = $facebook->getAccessToken();
-  print_r($access_token);
+
+
+ // auth user
+ if(empty($code)) {
+    $dialog_url = 'https://www.facebook.com/dialog/oauth?client_id=' 
+    . $app_id . '&redirect_uri=' . urlencode($my_url) ;
+    echo("<script>top.location.href='" . $dialog_url . "'</script>");
+  }
+
+  // get user access_token
+  $token_url = 'https://graph.facebook.com/oauth/access_token?client_id='
+    . $app_id . '&redirect_uri=' . urlencode($my_url) 
+    . '&client_secret=' . $app_secret 
+    . '&code=' . $code;
+
+  // response is of the format "access_token=AAAC..."
+  $access_token = substr(file_get_contents($token_url), 13);
+
+  // run fql query
+  $fql_query_url = 'https://graph.facebook.com/'
+    . 'fql?q=SELECT+uid+FROM+user+WHERE+uid+IN+(SELECT+uid2+FROM+friend+WHERE+uid1='.$user_id.')'
+    . '&access_token=' . $access_token;
+  $fql_query_result = file_get_contents($fql_query_url);
+  $fql_query_obj = json_decode($fql_query_result, true);
+
+  // display results of fql query
+  echo '<pre>';
+  print_r("query results:");
+  print_r($fql_query_obj);
+   echo '</pre>';
+
 
 ?>
-<html>
-  <head></head>
-  <body>
-
-  <?php
-    if($user_id) {
-
-$friends = $facebook->api('/'.$user_id.'/friends');
-                $friendsList = array();
-                foreach ($friends as $key=>$value) 
-                {
-                   foreach ($value as $fkey=>$fvalue) {
-
-                       $friendsList[] = $fvalue[id];
-//print_r($friendsList);
-                     //$fname= $facebook->api('/'.$fvalue[id].'?fields=name');  
-                     //print_r($fname);    
-                     //$fpic= $facebook->api('/'.$fvalue[id].'?fields=birthday');  
-                     //print_r($fpic);               
-                     
-                   }
-
-                }  
-      
-      // $friendsbday = $facebook->api('/'.$user_id.'?fields=friends.fields(birthday)?access_token='.$access_token.'?scope=friends_birthday'); 
-    $friendsbday="https://graph.facebook.com/".$user_id."/friends.fields(birthday)?access_token=".$access_token;  
-       echo $friendsbday;
-
-foreach($all_friends_profile as $profile)
-{
-   echo $profile->name.' birthay='.$profile->birthday_date;                  
-}
-
-      
-      
-    } else {
-
-      // No user, print a link for the user to login
-      $login_url = $facebook->getLoginUrl(array(
-                'scope'         => 'email,offline_access,publish_stream,user_birthday,user_location,user_work_history,user_about_me,user_hometown'
-            ));
-       echo 'Please <a href="' . $login_url . '">login.</a>';
-
-    }
-
-  ?>
-
-  </body>
-</html>
