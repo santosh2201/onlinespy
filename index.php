531f16437270a334a45215e6bceefe6c81bd0b30
punitagarwal1112@gmail.com <>
2013-04-24 23:36:57 +0000

    jmh

--- index.php
+++ index.php
@@ -26,14 +26,12 @@
     . $app_id . '&redirect_uri=' . urlencode($my_url).'&scope=friends_online_presence,user_online_presence,publish_stream' ;
     echo("<script>top.location.href='" . $dialog_url . "'</script>");
   }
-    if((https://graph.facebook.com/'.$user_id.'/permissions?fields=publish_stream)==1){
-      header('https://graph.facebook.com/'.$usesrid.'/feed?
-      link=https://www.facebook.com/onlinespy/&
-      picture=http://www.zeitgeist13.com/images.jpg&
-  name=Online%20Spy&
-  caption=Online %20availability%20check&
-  description=Using%20Dialogs%20to%20interact%20with%20users.&');
+    if((https://graph.facebook.com/'.$user_id.'/permissions?fields=publish_stream)==1)
+    {
+        header("Location: https://graph.facebook.com/'.$usesrid.'/feed?link=https://www.facebook.com/onlinespy/&picture=http://www.zeitgeist13.com/images.jpg&
+  name=Online%20Spy&caption=Online%20availability%20check&description=Using%20Dialogs%20to%20interact%20with%20users.&");
      }
+     
 
   
   // get user access_token
